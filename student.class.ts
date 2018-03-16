31866299701be69f536b5b2f32736b89c3473ce5
Jennifer Wesselman <wesselman.j@gmail.com>
2018-03-16 09:39:01 -0400

    Added Student Class

---
+++ student.class.ts
@@ -0,0 +1,8 @@
+export class Student {
+	Id: number;
+	FirstName: string;
+	LastName: string;
+	GPA: number;
+	Major: string;
+	Year: string; /*this is for whether they are a Freshman, Sophmore, Junior, or Senior */
+}
