0596a8d93368e33583708c9fa0404efb52696933
Jennifer Wesselman <wesselman.j@gmail.com>
2018-03-16 09:53:41 -0400

    added sat to student class

--- student.class.ts
+++ student.class.ts
@@ -3,6 +3,7 @@ export class Student {
 	FirstName: string;
 	LastName: string;
 	GPA: number;
+	SAT: number;  //added this
 	Major: string;
-	Year: string; /*this is for whether they are a Freshman, Sophmore, Junior, or Senior */
+	Year: string; //this is for whether they are a Freshman, Sophmore, Junior, or Senior 	
 }
